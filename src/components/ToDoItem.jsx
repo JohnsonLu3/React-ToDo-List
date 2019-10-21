fe48aab9ea56b87587ce9002430dbaba00d419d0
JohnsonLu3 <JohnsonLu94@gmail.com>
2019-10-21 17:57:33 -0400

    added remove and delete

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -4,6 +4,7 @@ import '../style/Main.scss';
 
 class ToDoItem extends React.Component {
 	
+	id;
 	todoID;
 	checkboxID;
 	trashButtID;
@@ -12,35 +13,35 @@ class ToDoItem extends React.Component {
 	constructor(props){
 		super(props);
 		
+		this.id = this.props.ToDoIndex;
 		this.todoID = "toDo_" + this.props.ToDoIndex;
 		this.checkboxID = "checkBox_" + this.props.ToDoIndex;
 		this.trashButtID = "trashButt_" + this.props.ToDoIndex;
-		this.checked = this.props.Checked;
+		this.checked = this.props.ToDoChecked;
 	}
 	
 	render(){
-			return (
+					
+		return (
 				<li id={this.todoID}>
 					
 					{
 						this.checked ? 
-							(<input id={this.checkboxID} type="checkbox" checked></input>)
+							(<input id={this.checkboxID} type="checkbox" checked onChange={this.props.markChecked.bind(this, this.id)} />)
 						:
-							(<input id={this.checkboxID} type="checkbox"></input>)
+							(<input id={this.checkboxID} type="checkbox" onChange={this.props.markChecked.bind(this, this.id)} />)
 					}
 					
 					<label for={this.checkboxID}>{this.props.ToDoValue}</label>
 
-					<button onClick={this.deleteItem} id={this.trashButtID} value={this.checkboxID} className="trashButt">
+					<button onClick={this.props.deleteItem.bind(this, this.id)} id={this.trashButtID} value={this.checkboxID} className="trashButt">
 						<p>X</p>
 					</button>
 				</li>
 		 );
 	}
 
-	deleteItem(){
-		console.log(this.todoID);
-	}
+
 }
 
 export default ToDoItem;
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -3,26 +3,63 @@ import '../style/App.scss';
 import '../style/Main.scss';
 import ToDoItem from './ToDoItem.jsx'
 
-var ToDo = [
-	{val : "Buy Milk", checked : false},
-	{val : "The the Dog a bath", checked : false},
-	{val : "Take out the trash", checked : false},
-	{val : "Program a ToDo List", checked : true},
-]
+class ToDoList extends React.Component {
 
+	ToDo;
+	
+	state = {
+		ToDo : [
+				{val : "Buy Milk", checked : false},
+				{val : "The the Dog a bath", checked : false},
+				{val : "Take out the trash", checked : false},
+				{val : "Program a ToDo List", checked : true},
+			]
+	}
 
-function ToDoList(){
-	 return (
-		 <div id="ToDoList">
-			 	<ul>
-					{
-						ToDo.map((value, index) =>{
-							return <ToDoItem key={"ToDO_Key"+index} ToDoValue={value.val} Checked={value.checked} ToDoIndex={index}/>	
-						})
-					}
-				 </ul>
-		 </div>
-	 );
+	constructor(props){
+			super(props);
+
+			
+	}
+	
+	render(){
+		return (
+			 <div id="ToDoList">
+					<ul>
+						{
+							this.state.ToDo.map((value, index) =>{
+								return <ToDoItem key={"ToDO_Key"+index} ToDoValue={value.val} ToDoChecked={value.checked} ToDoIndex={index} markChecked={this.markChecked} deleteItem={this.deleteItem}/>	
+							})
+						}
+					 </ul>
+			 </div>
+		 );
+	}
+	
+	markChecked = (id) =>{
+		console.log("Mark Checked: " + id);
+		
+		this.state.ToDo.map((item, index) =>{
+			if(index === id){
+			   console.log(item);
+				if (item.checked){
+					item.checked = false;
+				}else{
+					item.checked = true;
+				}
+			}
+			return;
+		});	
+		this.setState({ ToDo : this.state.ToDo});
+	}
+	
+	deleteItem = (id) =>{
+		console.log("Delete Item: " + id);
+		
+		this.state.ToDo.splice(id,1)
+		this.setState({ ToDo : this.state.ToDo});
+		
+		return;
+	}
 }
-
 export default ToDoList;
